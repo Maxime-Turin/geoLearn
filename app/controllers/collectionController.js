52d820b52850e2576394052a23c6def224ef2342
Maxime Turin <maxime.turin@gmail.com>
2023-01-19 15:58:50 +0100

    commentaires

--- app/controllers/collectionController.js
+++ app/controllers/collectionController.js
@@ -3,16 +3,19 @@ const { capitalizeFirstLetter, minimizeFirstLetter } = require('../helpers');
 
 module.exports = {
   async getAllDocuments(req, res) {
-    const { collectionName } = req.params;
+    let { collectionName } = req.params;
+    // Adapte les premières lettre au "modèle" de la bdd
+    collectionName = minimizeFirstLetter(collectionName);
     const result = await models.document.findAllDocuments(collectionName);
     res.json(result);
   },
 
   async getOneDocumentByName(req, res) {
     let { collectionName, documentName } = req.params;
+    // Adapte les premières lettre au "modèle" de la bdd
     collectionName = minimizeFirstLetter(collectionName);
     documentName = capitalizeFirstLetter(documentName);
-    console.log(collectionName + documentName);
+
     const result = await models.document.findDocumentByName(collectionName, documentName);
     res.json(result);
   },
